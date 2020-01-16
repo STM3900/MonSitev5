3b79ee2da1b7c8f28b8a869a4befb9aa776d717c
Théo <theo_38180@live.fr>
2020-01-16 12:03:46 +0100

    Partie projets terminée !

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ Overlay.addEventListener("click", closeNav);
 
 NavButton.addEventListener("click", function(){
     openNav();
-    setTimeout(function(){
+    setTimeout(() => {
         NavButton.style.opacity = 0;
     }, 100);
 });
@@ -47,3 +47,83 @@ NavButton.addEventListener("mouseout", function(){
     NavButton.style.opacity = 0.4;
 });
 
+const Projets = document.querySelectorAll('.projets');
+const ProjetsOverlay = document.querySelector('.projetOverlay');
+const ProjetsListe = document.querySelectorAll('.projetsListe article');
+const Panel = document.querySelectorAll('.panel');
+const PanelFlex = document.querySelectorAll('.panelFlex');
+const PanelClose = document.querySelectorAll('.panelclose');
+
+
+let panelOn = false;
+
+for(let i = 0; i < ProjetsListe.length; i++){
+    PanelClose[i].addEventListener("click", closePanel);
+    ProjetsListe[i].addEventListener("click", function(){
+        PanelFlex[i].style.transition = "0.3s";
+        for(let y = 0; y < ProjetsListe.length; y++){
+            ProjetsListe[y].style.opacity = 0;
+            setTimeout(() => {
+                ProjetsListe[y].style.zIndex = -1;
+            }, 300);
+        }
+        ProjetsOverlay.style.zIndex = 10;
+        Panel[i].className = "panelok";
+        setTimeout(() => {
+            PanelFlex[i].style.opacity = 1;
+        }, 300);
+        setTimeout(() => {
+            panelOn = true;
+        }, 0);
+    });
+};
+
+function closePanel(){
+    panelOn = false;
+    ProjetsOverlay.style.zIndex = -1;
+    for(let i = 0; i < ProjetsListe.length; i++){
+        ProjetsListe[i].style.zIndex = 10;
+        ProjetsListe[i].style.opacity = 1;
+        PanelFlex[i].style.transition = "none";
+        PanelFlex[i].style.opacity = 0;
+        Panel[i].className = "panel";
+    }
+}
+
+
+ProjetsOverlay.addEventListener("click", function(){
+    if(panelOn == true){
+        closePanel();
+    }        
+});
+
+//Le convertisseur
+function convDecimal()
+{
+    decimal = document.getElementById("decimal");
+    hexadecimal = document.getElementById("hexadecimal");
+    binaire = document.getElementById("binaire");
+    
+    hexadecimal.value = (decimal.value-0).toString(16);
+    binaire.value = (decimal.value-0).toString(2);
+}
+
+function convHexadecimal()
+{
+    decimal = document.getElementById("decimal");
+    hexadecimal = document.getElementById("hexadecimal");
+    binaire = document.getElementById("binaire");
+    
+    decimal.value = parseInt(hexadecimal.value,16);
+    binaire.value = (parseInt(hexadecimal.value,16)).toString(2);
+}
+
+function convBinaire()
+{
+    decimal = document.getElementById("decimal");
+    hexadecimal = document.getElementById("hexadecimal");
+    binaire = document.getElementById("binaire");
+    
+    decimal.value = parseInt(binaire.value,2);
+    hexadecimal.value = (parseInt(binaire.value,2)).toString(16);
+}
